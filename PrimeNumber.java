0bc962570789aaf43551219c02b4985e40907c28
bipinrajak <rajakbipin2004@gmail.com>
2026-06-10 01:42:04 +0530

    Java code

---
+++ PrimeNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class PrimeNumber{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your number");
+        int num = input.nextInt();
+        boolean isPrime=true;
+        if(num == 1){
+            System.out.print("1 is neither prime nor composite number");
+            return;
+        }
+        for(int i=2;i<=num/2;i++){
+            if(num%i==0){
+                isPrime = false;
+            }
+        }    
+        if(isPrime){
+            System.out.println(num+" is a prime number");
+        }
+        else{
+            System.out.println(num+" is not a prime number");
+        }
+        input.close();
+    }
+}
