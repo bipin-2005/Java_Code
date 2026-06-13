f5c8f5749e6233570028a401b0610ff27bbe9806
bipinrajak <rajakbipin2004@gmail.com>
2026-06-14 01:56:08 +0530

    inverseNumber problem

---
+++ Numbers_Question/InverseNumber.java
@@ -0,0 +1,21 @@
+package Numbers_Question;
+import java.util.Scanner;
+public class InverseNumber{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your number : ");
+        int num = input.nextInt();
+        int inverse=0,position=1;
+        while(num!=0){
+            int original_digit = num%10;
+            int inverse_digit = position;
+            int inverse_position = original_digit;
+
+            inverse = inverse + inverse_digit * (int)Math.pow(10,inverse_position-1);
+
+            num/=10;
+            position++;
+        }
+        System.out.print("Inverse number is : "+inverse);
+    }
+}
